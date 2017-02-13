b770b617ae9c9d6209324a1a2463d131645c80e5
Nick Jones <njhazelh@gmail.com>
2017-02-13 17:28:54 -0500

    MBTA-18: Create mock app that logs every 5 minutes
    The value of this mock app is that once we get the deployment infrastructure working, we'll be able to know that the app is still running.

--- archiver.py
+++ archiver.py
@@ -1,1 +1,17 @@
-# ... download new alert data and write to database ...
+import os
+import logging
+import time
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+LOG = logging.getLogger("archive")
+
+
+def main():
+    while True:
+        LOG.info("still running")
+        # Sleep for 1 minute
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    main()
