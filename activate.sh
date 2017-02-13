7c9455689092ad31ef4f975541f3f885d330dcc5
Nick Jones <njhazelh@gmail.com>
2017-02-13 18:40:46 -0500

    Allow minor versions >= 3.4

--- activate.sh
+++ activate.sh
@@ -16,7 +16,7 @@ main() {
     command -v python3 >/dev/null 2>&1 || { echo >&2 "I require python3 but it's not installed.  Aborting."; exit 1; }
     let python_major_version="$(python3 -c 'import sys; print(sys.version_info[0])')"
     let python_minor_version="$(python3 -c 'import sys; print(sys.version_info[1])')"
-    if [[ "$python_major_version" -ne "3" || "$python_minor_version" -ne "4" ]]; then
+    if [[ "$python_major_version" -ne "3" || "$python_minor_version" -lt "4" ]]; then
         echo "This project is dependent on python3.4, but you're using $python_major_version . $python_minor_version"
         exit 1
     fi
