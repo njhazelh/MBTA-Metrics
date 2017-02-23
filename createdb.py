e33572d0954de8211a073ffc50a3766f5889f958
Tiffany Chao <tifachao@gmail.com>
2017-02-23 16:35:56 -0500

    instructions to connect to db in README

--- createdb.py
+++ createdb.py
@@ -8,11 +8,15 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 # read from settings file
 config = configparser.ConfigParser()
 config.read("settings.cfg")
+host = config.get('Database', 'host')
+port = config.get('Database', 'port')
+db = config.get('Database', 'db')
 user = config.get('Database', 'user')
 passwd = config.get('Database', 'pass')
 
 # set up connection
-engine = sqlalchemy.create_engine('postgresql://' + user + ':' + passwd +'@localhost:5432/postgres')
+engine = sqlalchemy.create_engine('postgresql://' + user + ':' + passwd + '@'
+                                  + host + ':' + port + '/' + db)
 connection = engine.connect()
 
 
