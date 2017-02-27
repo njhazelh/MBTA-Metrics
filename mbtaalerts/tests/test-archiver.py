6e493e9f09cfe439d6199ceeb2cefc3338d6cc0f
Nick Jones <njhazelh@gmail.com>
2017-02-27 18:14:29 -0500

    MBTA-26: Fixup code to meet pylint expectations

--- mbtaalerts/tests/test-archiver.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import mbtaalerts.archiver
-
-class ArchiverTest(unittest.TestCase):
-    def test_example(self):
-        self.assertTrue(True)
-
-    def test_example2(self):
-       self.assertEqual(1, mbtaalerts.archiver.helper_function())
---
+++ mbtaalerts/tests/test_digest.py
@@ -0,0 +1,15 @@
+"""
+This module will test the digest module.
+"""
+
+import unittest
+
+class DigestTester(unittest.TestCase):
+    """
+    Perform tests on digest module
+    """
+    def test_example(self):
+        """
+        Check that 1 + 1 == 2
+        """
+        self.assertEqual(1 + 1, 2)
