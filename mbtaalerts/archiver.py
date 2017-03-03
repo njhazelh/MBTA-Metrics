b7d756a08e2cf7b3f7c86547f86ae9284e2c1ef7
Yuvi <yuvi.shatil@gmail.com>
2017-03-03 15:09:57 -0500

    Removing unused helper function in archiver. Printing json instead of request object.

--- mbtaalerts/archiver.py
+++ mbtaalerts/archiver.py
@@ -6,13 +6,10 @@ import requests
 logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s %(message)s', datefmt='%m/%d %H:%M:%S', level=os.environ.get("LOGLEVEL", "INFO"))
 LOG = logging.getLogger("archive")
 
-def helper_function():
-    return 1
-
 def main():
     requestUrl = "http://realtime.mbta.com/developer/api/v2/alerts?api_key=wX9NwuHnZU2ToO7GmGR9uw&include_access_alerts=true&include_service_alerts=true&format=json"
     r = requests.get(requestUrl)
-    print(r)
+    print(r.json())
     while True:
         LOG.info("still running")
         # Sleep for 1 minute
