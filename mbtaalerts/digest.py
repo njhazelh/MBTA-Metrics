00902e293760ebac41c64fd86d0f0d8260faacd7
yuvi <yuvi@ccs.neu.edu>
2017-03-29 16:38:58 -0400

    Class hierarchy

--- mbtaalerts/digest.py
+++ mbtaalerts/digest.py
@@ -1,1 +1,15 @@
-# ... calculate alert accuracy and generate a plaintext report ...
+class AlertGrouping:
+    def __init__(self, alert_obj, event_list):
+        self.alert_obj = alert_obj
+        self.event_list = event_list
+
+    def __init__(self, event_list):
+        self.event_list = event_list
+
+class AlertObj:
+    def __init__(self, alert_obj):
+        self.alert_obj = alert_obj
+
+class event:
+    def __init__(self, json):
+        self.event = json
