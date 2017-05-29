f9e324567b9fa01ac07e939ccd5fe5d2c1a0b9ee
Nick Jones <njhazelh@gmail.com>
2017-05-29 03:13:12 -0400

    Catch some errors that weren't being caught
    It seems like the archiver hadn't been running for a while, because
    it encountered a Integrity error with a duplicate key.  I had hoped
    we were done with duplicate key issues, but it seems like there's
    still a way to go.  At least this way, it won't kill the entire
    application.

--- mbtaalerts/archiver.py
+++ mbtaalerts/archiver.py
@@ -4,21 +4,18 @@ the MBTA APIs that contain relevant data and archiving them
 to our database.
 """
 
-import os
 import time
 import requests
 from datetime import datetime, timezone
 
-from sqlalchemy.exc import DBAPIError
+from sqlalchemy.exc import SQLAlchemyError
+import psycopg2
 
 from mbtaalerts.logging import get_log
 from mbtaalerts.config import config as cfg
 from mbtaalerts.database.database import get_db_session
 from mbtaalerts.database.models import \
     Alert, AlertAffectedService, AlertEffectPeriod
-
-DB_SESSION = get_db_session()
-LOG = get_log('archiver')
 
 
 def buildAlertEntry(alert):
@@ -85,6 +82,8 @@ class Archiver:
             'format': 'json'
         }
         self.alerts_url = cfg.get("API", "alerts_url")
+        self.db_session = get_db_session()
+        self.log = get_log('archiver')
 
     def storeAlerts(self, alerts_info):
         """Convenience method for preparing and then executing the update operation."""
@@ -150,7 +149,7 @@ class Archiver:
 
         # Ignore duplicate alerts
         if not is_duplicate:
-            LOG.info("Adding alert with ID %s", alert_id)
+            self.log.info("Adding alert with ID %s", alert_id)
             # Mark this alert version as 'seen' before processing it,
             # so we don't get stuck re-processing it if it fails
             self.known_id_lastmodified_pairs.add((alert_id, last_modified))
@@ -171,15 +170,15 @@ class Archiver:
         new_services_num = len(service_insertions)
         new_periods_num = len(effect_period_insertions)
         if new_alerts_num > 0:
-            LOG.info("Inserting %d alerts.", new_alerts_num)
-            DB_SESSION.add_all(alert_insertions)
+            self.log.info("Inserting %d alerts.", new_alerts_num)
+            self.db_session.add_all(alert_insertions)
         if new_services_num > 0:
-            LOG.info("Inserting %d affected services.", new_services_num)
-            DB_SESSION.add_all(service_insertions)
+            self.log.info("Inserting %d affected services.", new_services_num)
+            self.db_session.add_all(service_insertions)
         if new_periods_num > 0:
-            LOG.info("Inserting %d alert affect periods.", new_periods_num)
-            DB_SESSION.add_all(effect_period_insertions)
-        DB_SESSION.commit()
+            self.log.info("Inserting %d alert affect periods.", new_periods_num)
+            self.db_session.add_all(effect_period_insertions)
+        self.db_session.commit()
 
     def updateKnownAlerts(self, alerts_info):
         """Remove unpublished alerts from the known alert set"""
@@ -203,11 +202,11 @@ class Archiver:
         # Remove the pairs from our set which have currently-published IDs
         for pair in items_to_cull:
             self.known_id_lastmodified_pairs.discard(pair)
-        LOG.info("Purging %d unpublished alerts from known alert set", len(items_to_cull))
+        self.log.info("Purging %d unpublished alerts from known alert set", len(items_to_cull))
 
     def run(self):
         while True:
-            LOG.info("Scanning for new alerts")
+            self.log.info("Scanning for new alerts")
             try:
                 response = requests.get(self.alerts_url, params=self.alerts_params)
                 alerts_info = response.json()
@@ -215,16 +214,17 @@ class Archiver:
                 self.storeAlerts(alerts_info)
             except requests.ConnectionError:
                 # This may just indicate a temporary API outage
-                LOG.exception("Unable to retrieve alerts data from API")
+                self.log.exception("Unable to retrieve alerts data from API")
             except KeyError:
                 # Should only happen after problematic alerts are marked as known
                 # so it's safe to continue scanning (we won't get stuck re-processing them).
-                LOG.exception("Couldn't find required field in alerts response")
-            except DBAPIError:
+                self.log.exception("Couldn't find required field in alerts response")
+            except (SQLAlchemyError, psycopg2.Error, psycopg2.Warning):
                 # Should only happen after problematic alerts are marked as known
                 # so it's safe to continue scanning (we won't get stuck re-processing them).
-                LOG.exception("Encountered an error when performing database insertion")
-            LOG.info("Finished scanning new alerts")
+                self.log.exception("Encountered an error when performing database insertion")
+                self.db_session.rollback()
+            self.log.info("Finished scanning new alerts")
             time.sleep(self.scan_frequency_sec)
 
 
